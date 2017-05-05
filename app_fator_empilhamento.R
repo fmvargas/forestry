567d61b0140da45a959518d39cc57ad19e1d7172
Filipe Vargas <filipemarcelvargas@gmail.com>
2017-05-05 15:57:03 -0300

    Update app_fator_empilhamento.R

--- app_fator_empilhamento.R
+++ app_fator_empilhamento.R
@@ -8,10 +8,8 @@ img <- imager::load.image("imagem.jpg")
 imgDm <- dim(img)
 img <- imager::resize(img, imgDm[1]/par$V2[1], imgDm[2]/par$V2[1])
 
-# Obtain the dimension
 imgDm <- dim(img)
 
-# Assign RGB channels to data frame
 imgRGB <- data.frame(
         x = rep(1:imgDm[2], each = imgDm[1]),
         y = rep(imgDm[1]:1, imgDm[2]),
@@ -22,7 +20,6 @@ imgRGB <- data.frame(
 
 library(ggplot2)
 
-# ggplot theme to be used
 plotTheme <- function() {
         theme(
                 panel.background = element_rect(
@@ -50,28 +47,17 @@ plotTheme <- function() {
         )
 }
 
-# Plot the image
-#ggplot(data = imgRGB, aes(x = x, y = y)) + 
-#        geom_point(colour = rgb(imgRGB[c("R", "G", "B")])) +
-#        labs(title = "Original Image") +
-#        xlab("x") +
-#        ylab("y") +
-#        plotTheme()
 
-##Clustering
 
-#Apply k-Means clustering on the image:
 kClusters <- par$V2[2]
 kMeans <- kmeans(imgRGB[, c("R", "G", "B")], centers = kClusters)
 kColours <- rgb(kMeans$centers[kMeans$cluster,])
 
-#fator de empilhamento:
 fe <- sum(kMeans$size)/sum(kMeans$size[1:2])
 
-#Plot the clustered colours:
 imagem <- ggplot(data = imgRGB, aes(x = x, y = y)) + 
         geom_point(colour = kColours) +
-        labs(title = paste("Classificação em", kClusters, "cores")) +
+        labs(title = paste("ClassificaÃ§Ã£o em", kClusters, "cores")) +
         labs(x = paste("Fator de empilhamento:", fe)) +
         plotTheme()
 
